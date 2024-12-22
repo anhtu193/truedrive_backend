88ae096e720e65dd5731a7db47b8c169a57612a7
hungcqa23 <anbeel191@gmail.com>
2024-12-22 18:42:44 +0700

    add showroom, appointment api

---
+++ truedrive_backend/Controllers/AppointmentController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using truedrive_backend.Data;
+using truedrive_backend.Models;
+
+namespace truedrive_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppointmentController : ControllerBase
+    {
+        private readonly TrueDriveContext _context;
+
+        public AppointmentController(TrueDriveContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Appointment
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Appointment>>> GetAppointments()
+        {
+            return await _context.Appointment.ToListAsync();
+        }
+
+        // GET: api/Appointment/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Appointment>> GetAppointment(int id)
+        {
+            var appointment = await _context.Appointment.FindAsync(id);
+
+            if (appointment == null)
+            {
+                return NotFound();
+            }
+
+            return appointment;
+        }
+
+        // POST: api/Appointment
+        [HttpPost]
+        public async Task<ActionResult<Appointment>> PostAppointment(Appointment appointment)
+        {
+            _context.Appointment.Add(appointment);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetAppointment), new { id = appointment.AppointmentId }, appointment);
+        }
+
+        // PUT: api/Appointment/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAppointment(int id, Appointment appointment)
+        {
+            if (id != appointment.AppointmentId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(appointment).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AppointmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/Appointment/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAppointment(int id)
+        {
+            var appointment = await _context.Appointment.FindAsync(id);
+            if (appointment == null)
+            {
+                return NotFound();
+            }
+
+            _context.Appointment.Remove(appointment);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AppointmentExists(int id)
+        {
+            return _context.Appointment.Any(e => e.AppointmentId == id);
+        }
+    }
+}
---
+++ truedrive_backend/Models/Appointment.cs
@@ -0,0 +1,15 @@
+﻿namespace truedrive_backend.Models
+{
+    public class Appointment
+    {
+        public int AppointmentId { get; set; }
+        public int CustomerId { get; set; }
+        public int CarId { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public string Showroom { get; set; }
+        public string Status { get; set; }
+        public string Purpose { get; set; }
+        public string Note { get; set; }
+    }
+}
